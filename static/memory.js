96defb0dda8caec7342e4d150f7616952a31695d
Oshane <b4.oshany@gmail.com>
2014-03-28 02:00:04 -0500

    added user profile

--- static/memory.js
+++ static/memory.js
@@ -2,6 +2,37 @@
 A = "position:absolute;";
 W = "width:120px;height:160px;";
 var audioElement = document.getElementById("audio");
+var username = document.querySelector("#profilename");
+var userturn = document.querySelector("#turns")
+var userpic = document.querySelector("#gamer_photo")
+var supportStorage = false;
+var userdata = {"username": null, "turn": 0, "photo": null};
+
+function saveData(userdata){
+    if(supportStorage)
+        localStorage.gamer = JSON.stringify(userdata)
+}
+
+if(typeof(Storage) !== undefined){
+    supportStorage = true;
+    console.log("OK");
+    console.log(localStorage.gamer);
+    if(localStorage.gamer === undefined)
+        saveData(userdata);
+    else{
+        userdata = JSON.parse(localStorage.gamer); 
+        if(userdata.username != null) username.innerHTML = "Hi, "+userdata.username;
+        if(userdata.photo != null) userpic.setAttribute("src", userdata.photo);        
+    }                
+}
+
+document.onload = function(){
+    if(userdata.username != null) username.innerHTML = userdata.username;
+    displayTurn(userdata.turn)
+    if(userdata.photo != null) userpic.setAttribute("src", userdata.photo);   
+    saveData(userdata);    
+}
+
 
 function C(e, c) {
     e.className = "w " + c
@@ -16,7 +47,7 @@ function R(a) {
 }
 
 function S(y){
-    if(y){ alert("OK");
+    if(y){
         audioElement.setAttribute("src", "/static/sound/flip.mp3");
         audioElement.play();
     }else{        
@@ -33,7 +64,9 @@ var hide = ".c.b { -webkit-transform-style: preserve- 3 d;  -webkit-backface-vis
 
 document.querySelector("head").innerHTML = "<style>.w{" + W + "position: relative; "+P("perspective: 700")+"border:1px solid #777;text-align:center;margin:5px;float:left" + "}.v .b,.p .b{" + P("transform:scale(0)") + "}.c{" + W + A + "font-size:80px}b{" + A + "font-size:24px;left:5px}.b,.r{color:red}.b{background: black url(http://jbrown72.files.wordpress.com/2010/12/yugioh-card-back.jpg) no-repeat; background-size: cover; " + P("transition:1s") +"} .txt{position: absolute; top: 30%; ;left: 10%;} "+hide+show+"</style>";
 
-var turn = 1; var clicks = 0;
+var turn = (supportStorage && !isNaN(userdata.turn))? parseInt(userdata.turn): 0;
+userturn.innerHTML = turn;
+var clicks = 0;
 function RS(){ 
 	var con = confirm("Do you want to start a new game?");
 	if(con){b.innerHTML = ""; D(); turn = 1; clicks = 0;}
@@ -42,13 +75,18 @@ function RS(){
 function F(t) {
 	console.log("turn "+turn);
 	clicks++;
-	if(turn == 5){ RS(); return};
+	if(turn == 24){ RS(); return};
     v = b.querySelectorAll(".v");
     x = v[0];
     y = v[1];
     if (y) C(x, ""), C(y, "");
     if (x && !y && x != t && x.innerHTML == t.innerHTML) C(x, "p"), C(t, "p"), l--;
-    else C(t, "v"), (clicks%2==0)?turn++:''; 
+    else{ 
+        C(t, "v"); (clicks%2==0)?turn++:'';
+        if(supportStorage) userdata.turn = turn;
+        saveData(userdata);
+        userturn.innerHTML = turn; 
+    }
     S(true);
     if (!l) S(false), setTimeout( function(){alert("I ♥ U!")}, 2000) 
 }
@@ -66,5 +104,19 @@ function D(){
 	b.innerHTML = h + '</div>'
 }
 D();
+
+username.onclick=function(){
+    var usr = prompt("Please enter your username");
+    userdata.username = usr;
+    saveData(userdata);
+    if(usr != null) this.innerHTML = "Hi, "+usr;
+}
+
+userpic.onclick=function(){
+    var photo = prompt("Enter url for profile image");
+    userdata.photo = photo;
+    saveData(userdata);
+    if(photo != null) this.setAttribute("src", photo);
+}
 
 // end of submission //
